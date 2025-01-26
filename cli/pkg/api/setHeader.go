35d249bef2723bb9fda4d00f00c59e95c6c538e9
MarkDevOps <markaskey@hotmail.co.uk>
2025-01-26 21:36:40 +0000

    Removed old attempt

--- cli/pkg/api/setHeader.go
+++ cli/pkg/api/setHeader.go
@@ -9,7 +9,8 @@ func setHeader() string {
 	// checking GITHUB_TOKEN environment variable exists
 	GITHUB_TOKEN := os.Getenv("GITHUB_TOKEN")
 	if GITHUB_TOKEN == "" {
-		fmt.Errorf("GITHUB_TOKEN environment variable not set")
+		fmt.Println("GITHUB_TOKEN environment variable not set")
+		return "Error!!"
 	}
 	return GITHUB_TOKEN
 }
